402fe0aa9c5b3fca3ce43a40cb5e784135cfc762
James (Shoti) <163653982+James43233@users.noreply.github.com>
2026-01-15 09:21:43 +0800

    feat(router): added payment history and billing route

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -18,6 +18,7 @@ import { Route as AuthLoginRouteImport } from './routes/auth/login'
 import { Route as DashboardMarketingMembershipRouteImport } from './routes/dashboard/marketing/membership'
 import { Route as DashboardMarketingBranchRouteImport } from './routes/dashboard/marketing/branch'
 import { Route as DashboardMarketingAssetsRouteImport } from './routes/dashboard/marketing/assets'
+import { Route as DashboardBillingPaymentHistoryRouteImport } from './routes/dashboard/billing/payment-history'
 import { Route as DashboardAdminExpenseRouteImport } from './routes/dashboard/admin/expense'
 
 const PostRoute = PostRouteImport.update({
@@ -68,6 +69,12 @@ const DashboardMarketingAssetsRoute =
     path: '/marketing/assets',
     getParentRoute: () => DashboardRouteRoute,
   } as any)
+const DashboardBillingPaymentHistoryRoute =
+  DashboardBillingPaymentHistoryRouteImport.update({
+    id: '/billing/payment-history',
+    path: '/billing/payment-history',
+    getParentRoute: () => DashboardRouteRoute,
+  } as any)
 const DashboardAdminExpenseRoute = DashboardAdminExpenseRouteImport.update({
   id: '/admin/expense',
   path: '/admin/expense',
@@ -82,6 +89,7 @@ export interface FileRoutesByFullPath {
   '/auth/login': typeof AuthLoginRoute
   '/dashboard/sales': typeof DashboardSalesRoute
   '/dashboard/admin/expense': typeof DashboardAdminExpenseRoute
+  '/dashboard/billing/payment-history': typeof DashboardBillingPaymentHistoryRoute
   '/dashboard/marketing/assets': typeof DashboardMarketingAssetsRoute
   '/dashboard/marketing/branch': typeof DashboardMarketingBranchRoute
   '/dashboard/marketing/membership': typeof DashboardMarketingMembershipRoute
@@ -94,6 +102,7 @@ export interface FileRoutesByTo {
   '/auth/login': typeof AuthLoginRoute
   '/dashboard/sales': typeof DashboardSalesRoute
   '/dashboard/admin/expense': typeof DashboardAdminExpenseRoute
+  '/dashboard/billing/payment-history': typeof DashboardBillingPaymentHistoryRoute
   '/dashboard/marketing/assets': typeof DashboardMarketingAssetsRoute
   '/dashboard/marketing/branch': typeof DashboardMarketingBranchRoute
   '/dashboard/marketing/membership': typeof DashboardMarketingMembershipRoute
@@ -107,6 +116,7 @@ export interface FileRoutesById {
   '/auth/login': typeof AuthLoginRoute
   '/dashboard/sales': typeof DashboardSalesRoute
   '/dashboard/admin/expense': typeof DashboardAdminExpenseRoute
+  '/dashboard/billing/payment-history': typeof DashboardBillingPaymentHistoryRoute
   '/dashboard/marketing/assets': typeof DashboardMarketingAssetsRoute
   '/dashboard/marketing/branch': typeof DashboardMarketingBranchRoute
   '/dashboard/marketing/membership': typeof DashboardMarketingMembershipRoute
@@ -121,6 +131,7 @@ export interface FileRouteTypes {
     | '/auth/login'
     | '/dashboard/sales'
     | '/dashboard/admin/expense'
+    | '/dashboard/billing/payment-history'
     | '/dashboard/marketing/assets'
     | '/dashboard/marketing/branch'
     | '/dashboard/marketing/membership'
@@ -133,6 +144,7 @@ export interface FileRouteTypes {
     | '/auth/login'
     | '/dashboard/sales'
     | '/dashboard/admin/expense'
+    | '/dashboard/billing/payment-history'
     | '/dashboard/marketing/assets'
     | '/dashboard/marketing/branch'
     | '/dashboard/marketing/membership'
@@ -145,6 +157,7 @@ export interface FileRouteTypes {
     | '/auth/login'
     | '/dashboard/sales'
     | '/dashboard/admin/expense'
+    | '/dashboard/billing/payment-history'
     | '/dashboard/marketing/assets'
     | '/dashboard/marketing/branch'
     | '/dashboard/marketing/membership'
@@ -223,6 +236,13 @@ declare module '@tanstack/react-router' {
       preLoaderRoute: typeof DashboardMarketingAssetsRouteImport
       parentRoute: typeof DashboardRouteRoute
     }
+    '/dashboard/billing/payment-history': {
+      id: '/dashboard/billing/payment-history'
+      path: '/billing/payment-history'
+      fullPath: '/dashboard/billing/payment-history'
+      preLoaderRoute: typeof DashboardBillingPaymentHistoryRouteImport
+      parentRoute: typeof DashboardRouteRoute
+    }
     '/dashboard/admin/expense': {
       id: '/dashboard/admin/expense'
       path: '/admin/expense'
@@ -236,6 +256,7 @@ declare module '@tanstack/react-router' {
 interface DashboardRouteRouteChildren {
   DashboardSalesRoute: typeof DashboardSalesRoute
   DashboardAdminExpenseRoute: typeof DashboardAdminExpenseRoute
+  DashboardBillingPaymentHistoryRoute: typeof DashboardBillingPaymentHistoryRoute
   DashboardMarketingAssetsRoute: typeof DashboardMarketingAssetsRoute
   DashboardMarketingBranchRoute: typeof DashboardMarketingBranchRoute
   DashboardMarketingMembershipRoute: typeof DashboardMarketingMembershipRoute
@@ -244,6 +265,7 @@ interface DashboardRouteRouteChildren {
 const DashboardRouteRouteChildren: DashboardRouteRouteChildren = {
   DashboardSalesRoute: DashboardSalesRoute,
   DashboardAdminExpenseRoute: DashboardAdminExpenseRoute,
+  DashboardBillingPaymentHistoryRoute: DashboardBillingPaymentHistoryRoute,
   DashboardMarketingAssetsRoute: DashboardMarketingAssetsRoute,
   DashboardMarketingBranchRoute: DashboardMarketingBranchRoute,
   DashboardMarketingMembershipRoute: DashboardMarketingMembershipRoute,
